68517bbb141ba986c87031e04da84fb2a1c9cd61
Stephan J Roorda <sjroorda@gmail.com>
2020-11-11 16:09:49 -0500

    Scrimmag TeleOp code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -12,34 +12,31 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "Auto", group = "Mechanum")
 public class Auto extends LinearOpMode {
 
-    DcMotor m1, m2, m3, m4;
-    Servo grabber;
+    DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+    DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+    DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+    DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+    Servo grabber = hardwareMap.servo.get("grabber");
     //GyroSensor gyro;
     BNO055IMU imu;
     ColorSensor colorSensor;
     Servo backServo;
     public void runOpMode() {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        m2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        m3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        m4.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        grabber = hardwareMap.servo.get("grabber");
 
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(new BNO055IMU.Parameters());
-
+        //imu = hardwareMap.get(BNO055IMU.class, "imu");
+        //imu.initialize(new BNO055IMU.Parameters());
 
         waitForStart();
         ElapsedTime t = new ElapsedTime();
 
         grabber.setPosition(0.5);
         while (opModeIsActive()&& t.seconds()<=2.60) {
-            setFront(1);
+            setFront(0.5);
             telemetry.addData("Time", t.seconds());
 
         }
@@ -47,65 +44,41 @@ public class Auto extends LinearOpMode {
     }
 
     public void setFront(double Front) {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setPower(Front);
-        m2.setPower(Front);
-        m3.setPower(Front);
-        m4.setPower(Front);
+        frontLeft.setPower(-Front);
+        backLeft.setPower(-Front);
+        frontRight.setPower(Front);
+        backRight.setPower(Front);
 
     }
     public void setBack(double Back) {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setPower(-Back);
-        m2.setPower(-Back);
-        m3.setPower(-Back);
-        m4.setPower(-Back);
+        frontLeft.setPower(Back);
+        backLeft.setPower(Back);
+        frontRight.setPower(-Back);
+        backRight.setPower(-Back);
     }
     public void setLeft(double Left) {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setPower(Left);
-        m2.setPower(-Left);
-        m3.setPower(Left);
-        m4.setPower(-Left);
+        frontLeft.setPower(Left);
+        backLeft.setPower(-Left);
+        frontRight.setPower(Left);
+        backRight.setPower(-Left);
     }
     public void setRight(double Right) {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setPower(-Right);
-        m2.setPower(Right);
-        m3.setPower(-Right);
-        m4.setPower(Right);
+        frontLeft.setPower(-Right);
+        backLeft.setPower(Right);
+        frontRight.setPower(-Right);
+        backRight.setPower(Right);
     }
     public void setTurnL(double TurnL) {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setPower(-TurnL);
-        m2.setPower(-TurnL);
-        m3.setPower(TurnL);
-        m4.setPower(TurnL);
+        frontLeft.setPower(-TurnL);
+        backLeft.setPower(-TurnL);
+        frontRight.setPower(TurnL);
+        backRight.setPower(TurnL);
     }
     public void setTurnR(double TurnR) {
-        m1 = hardwareMap.dcMotor.get("frontLeft");
-        m2 = hardwareMap.dcMotor.get("backLeft");
-        m3 = hardwareMap.dcMotor.get("frontRight");
-        m4 = hardwareMap.dcMotor.get("backRight");
-        m1.setPower(TurnR);
-        m2.setPower(TurnR);
-        m3.setPower(-TurnR);
-        m4.setPower(-TurnR);
+        frontLeft.setPower(TurnR);
+        backLeft.setPower(TurnR);
+        frontRight.setPower(-TurnR);
+        backRight.setPower(-TurnR);
     }
 
 }
